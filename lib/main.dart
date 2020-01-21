b853923f7fff54b6edf14ee44743108ea8fe66ca
OmegaSkiller <boss.ivailo@gmail.com>
2020-01-21 15:36:29 +0200

    3 buttons

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var qIndex = 0;
+
   void happyFunc() {
-    print('Yes');
+    setState(() {
+      qIndex = 1;
+    });
   }
 
   void sadFunc() {
-    print('No');
+    setState(() {
+      qIndex = 2;
+    });
+  }
+
+  void resetFunc() {
+    setState(() {
+      qIndex = 0;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    var qsn = [
+      '',
+      'Yey, you are Sergo kydesnika',
+      'Take me back to Kydesnik!',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter App bar title'),
+          title: Text('--- Kydesnik --- (App)'),
         ),
         body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(qsn[qIndex]),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -35,6 +60,15 @@ class MyApp extends StatelessWidget {
               RaisedButton(
                 child: Text('No'),
                 onPressed: sadFunc,
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              RaisedButton(
+                child: Text('RESET'),
+                onPressed: resetFunc,
               )
             ],
           ),
